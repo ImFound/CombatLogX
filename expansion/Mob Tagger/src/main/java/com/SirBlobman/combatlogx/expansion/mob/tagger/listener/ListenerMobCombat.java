f64bc7b2f0d2871507c751f107c549924057c48e
SirBlobman <blobmansrealm@gmail.com>
2021-07-21 15:41:42 -0400

    10.4.2.2-Dev: Add more debug to mob tagger expansion.

--- expansion/Mob Tagger/src/main/java/com/SirBlobman/combatlogx/expansion/mob/tagger/listener/ListenerMobCombat.java
+++ expansion/Mob Tagger/src/main/java/com/SirBlobman/combatlogx/expansion/mob/tagger/listener/ListenerMobCombat.java
@@ -1,15 +1,17 @@
 package com.SirBlobman.combatlogx.expansion.mob.tagger.listener;
 
 import java.util.List;
-
-import com.SirBlobman.combatlogx.api.event.PlayerPreTagEvent;
-import com.SirBlobman.combatlogx.api.event.PlayerPreTagEvent.TagReason;
-import com.SirBlobman.combatlogx.api.event.PlayerPreTagEvent.TagType;
-import com.SirBlobman.combatlogx.api.utility.ICombatManager;
-import com.SirBlobman.combatlogx.expansion.mob.tagger.MobTagger;
+import java.util.logging.Logger;
 
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.entity.*;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.AnimalTamer;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
+import org.bukkit.entity.Tameable;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -20,8 +22,16 @@ import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.metadata.MetadataValue;
 import org.bukkit.projectiles.ProjectileSource;
 
-public class ListenerMobCombat implements Listener {
+import com.SirBlobman.combatlogx.api.ICombatLogX;
+import com.SirBlobman.combatlogx.api.event.PlayerPreTagEvent;
+import com.SirBlobman.combatlogx.api.event.PlayerPreTagEvent.TagReason;
+import com.SirBlobman.combatlogx.api.event.PlayerPreTagEvent.TagType;
+import com.SirBlobman.combatlogx.api.utility.ICombatManager;
+import com.SirBlobman.combatlogx.expansion.mob.tagger.MobTagger;
+
+public final class ListenerMobCombat implements Listener {
     private final MobTagger expansion;
+
     public ListenerMobCombat(MobTagger expansion) {
         this.expansion = expansion;
     }
@@ -51,17 +61,29 @@ public class ListenerMobCombat implements Listener {
     
     @EventHandler(priority=EventPriority.LOWEST, ignoreCancelled=true)
     public void beforeTag(PlayerPreTagEvent e) {
+        printDebug("Detected PlayerPreTagEvent...");
+
         LivingEntity enemy = e.getEnemy();
-        if(enemy == null || enemy instanceof Player) return;
-    
+        if(enemy == null || enemy instanceof Player) {
+            printDebug("Enemy is player or null, ignoring event.");
+            return;
+        }
+
+        printDebug("Checking if enemy EntityType is disabled...");
         if(checkMobTypeDisabled(enemy)) {
+            printDebug("Enemy EntityType is disabled, cancelling event.");
             e.setCancelled(true);
             return;
         }
-        
+
+        printDebug("Checking if enemy SpawnReason is disabled...");
         if(checkMobSpawnReasonDisabled(enemy)) {
+            printDebug("Enemy SpawnReason is disabled, cancelling event.");
             e.setCancelled(true);
+            return;
         }
+
+        printDebug("Finished PlayerPreTagEvents without cancellation.");
     }
     
     @EventHandler(priority=EventPriority.MONITOR, ignoreCancelled=true)
@@ -74,15 +96,15 @@ public class ListenerMobCombat implements Listener {
     }
     
     private boolean checkMobTypeDisabled(LivingEntity enemy) {
-        FileConfiguration config = this.expansion.getConfig("mob-tagger.yml");
-        boolean mobsDisabled = !config.getBoolean("tag-players");
+        FileConfiguration configuration = this.expansion.getConfig("mob-tagger.yml");
+        boolean mobsDisabled = !configuration.getBoolean("tag-players");
         if(mobsDisabled) return true;
 
         EntityType enemyType = enemy.getType();
         String enemyTypeName = enemyType.name();
-        List<String> mobTypeList = config.getStringList("mob-list");
+        List<String> mobTypeList = configuration.getStringList("mob-list");
 
-        boolean mobListInverted = config.getBoolean("mob-list-inverted");
+        boolean mobListInverted = configuration.getBoolean("mob-list-inverted");
         boolean containsAll = mobTypeList.contains("*");
         boolean containsEnemy = mobTypeList.contains(enemyTypeName);
         if(mobListInverted) return (containsAll || containsEnemy);
@@ -90,8 +112,8 @@ public class ListenerMobCombat implements Listener {
     }
     
     private boolean checkMobSpawnReasonDisabled(LivingEntity enemy) {
-        FileConfiguration config = this.expansion.getConfig("mob-tagger.yml");
-        List<String> spawnReasonList = config.getStringList("spawn-reason-list");
+        FileConfiguration configuration = this.expansion.getConfig("mob-tagger.yml");
+        List<String> spawnReasonList = configuration.getStringList("spawn-reason-list");
         if(spawnReasonList.contains("*")) return true;
         
         SpawnReason spawnReason = getSpawnReason(enemy);
@@ -142,5 +164,14 @@ public class ListenerMobCombat implements Listener {
         }
         
         return original;
+    }
+
+    private void printDebug(String message) {
+        ICombatLogX plugin = this.expansion.getPlugin();
+        YamlConfiguration configuration = plugin.getConfig("config.yml");
+        if(!configuration.getBoolean("debug")) return;
+
+        Logger logger = this.expansion.getLogger();
+        logger.info("[Debug] " + message);
     }
 }
